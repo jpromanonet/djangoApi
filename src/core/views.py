93360f7b0db35da66e487614a814eadbd5549aea
jpromanonet <programming@jpromano.net>
2022-09-15 20:19:51 -0300

    Json Response

--- src/core/views.py
+++ src/core/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # Third party imports
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 def test_view(request):
