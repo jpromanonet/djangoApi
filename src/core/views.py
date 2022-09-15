cb69e15c3ad204cd163c36f0c0994a21e4186a25
jpromanonet <programming@jpromano.net>
2022-09-15 20:22:06 -0300

    Json Response

--- src/core/views.py
+++ src/core/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-def test_view(request):
-    data = {
-        'name': 'John',
-        'age': 28
-    }
-    return JsonResponse(data, safe=False)
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name': 'Juan',
+            'age': 28
+        }
+        return Response(data)
