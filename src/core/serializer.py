383408fd2b4706ebfda126033a7eb819e8067eea
jpromanonet <programming@jpromano.net>
2022-09-22 18:15:26 -0300

    Almost done

--- src/core/serializer.py
+++ src/core/serializer.py
@@ -1,3 +1,4 @@
+from sqlite3 import Timestamp
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializer
 from django.db import models
@@ -5,3 +6,4 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
