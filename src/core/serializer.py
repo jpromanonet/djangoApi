e2577b51e3af203e53f2e22a2adc06c4e9e0fb1f
jpromanonet <programming@jpromano.net>
2022-09-22 18:18:37 -0300

    Almost done

--- src/core/serializer.py
+++ src/core/serializer.py
@@ -2,6 +2,7 @@ from sqlite3 import Timestamp
 from unittest.util import _MAX_LENGTH
 from rest_framework import serializer
 from django.db import models
+from django.db import get_user_model
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
